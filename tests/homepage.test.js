635a367a64a89be88da84203a2e22c597c3a2b4d
jainsparsh5 <jainsparsh17@gmail.com>
2026-08-30 16:47:20 +0530

    fix: address contributor hover review

--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -6,8 +6,8 @@ const root = path.resolve(__dirname, '..');
 const homepage = fs.readFileSync(path.join(root, 'index.html'), 'utf8');
 const enhancements = fs.readFileSync(path.join(root, 'assets/enhancements.css'), 'utf8');
 
-assert.match(homepage, /\.btn-contribute:hover\{[^}]*color:#fff;/);
-assert.doesNotMatch(enhancements, /a:hover\s*\{[^}]*color:[^;}]+!important/);
+assert.match(homepage, /\.btn-contribute:hover\s*\{[^}]*color:\s*#fff\s*;[^}]*\}/);
+assert.doesNotMatch(enhancements, /a:hover\s*\{[^}]*color:\s*[^;}]+!important[^}]*\}/);
 
 for (const contributor of [
   'DsThakurRawat',
@@ -19,7 +19,7 @@ for (const contributor of [
   'dhrumilbhut',
   'jainsparsh5',
 ]) {
-  assert.match(homepage, new RegExp(`github\\.com/${contributor}`));
+  assert.match(homepage, new RegExp(`href="https://github\\.com/${contributor}"`));
 }
 
 console.log('homepage checks passed');
